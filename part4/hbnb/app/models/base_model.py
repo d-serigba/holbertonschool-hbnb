96fecb8f269d7703e8eb1fdb81a3260980629255
d-serigba <sanka93777@gmail.com>
2026-04-09 10:51:35 +0200

    copied form part3

---
+++ part4/hbnb/app/models/base_model.py
@@ -0,0 +1,26 @@
+# app/models/base_model.py
+
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import uuid
+
+db = SQLAlchemy()
+
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    id = db.Column(db.String(60), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
